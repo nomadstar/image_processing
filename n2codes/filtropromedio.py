55429d11c179d095b741ecd0f665fc5a204dd4a1
N0m4d <108416813+nomadstar@users.noreply.github.com>
2025-05-13 11:01:22 -0400

    Clase 8

---
+++ n2codes/filtropromedio.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import scipy.ndimage as ndi
+import matplotlib.pyplot as plt 
+
+def filtro_media(A):
+    A = A[A > 0]  # Exclude zero values to avoid log(0)
+    #S = np.exp(np.mean(np.log(A))) Way 1
+    S = A.prod()**(1.0/A.size) # Way 2
+    return S
+
+
+img = cv2.imread('imagenes/noiseball.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#Ruido impulsional
+mat_noise=np.random.random(gray.shape); #creates a uniform random variable from 0 to 1 
+
+sp_noise_white= np.uint8(np.where(mat_noise>=0.9, 255,0))
+sp_noise_black= np.uint8(np.where(mat_noise>=0.1,  1,0))
+noise_img = cv2.multiply(gray,sp_noise_black)
+noise_img = cv2.add(noise_img,sp_noise_white)
+
+#aplicamos el filtro promedio alfa-acotado
+filtro= ndi.generic_filter(noise_img,filtro_media, [5,5])
+
+plt.figure()
+plt.imshow(noise_img, cmap="gray")
+plt.show()
+
+plt.figure()
+plt.imshow(filtro, cmap="gray")
+plt.show()
+
